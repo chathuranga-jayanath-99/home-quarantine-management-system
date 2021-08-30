b17b0bad2bab41eeaa80ddf750589d0613e4ab0f
chathuranga-jayanath-99 <chathuranga.19@cse.mrt.ac.lk>
2021-08-30 13:18:38 +0530

    add manage files

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php include('partials/menu.php');?>
+    
+    <!-- Main content starts -->
+
+    <!-- Main content ends -->
+
+    <!-- Footer starts -->
+    <div class="footer">
+        <div>
+            <p>All rights reserved.</p>
+        </div>
+    </div>
+    <!-- Footer ends -->
+
+</body>
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,52 @@
+<?php include('partials/menu.php');?>
+
+    <!-- Main content starts -->
+    <div class="main-content">
+
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+
+            <br><br>
+
+            <a href="#">Add Admin</a>
+
+            <br><br>
+
+            <table class="tbl-full">
+                
+                <tr>
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+                <!-- sample data starts -->
+                <tr>
+                    <td>1</td>
+                    <td>Chathuranga</td>
+                    <td>chathuwa</td>
+                    <td>
+                        <a href="#">Update</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+                <!-- sample data ends -->
+
+            </table>
+        </div>
+
+    </div>
+
+    <!-- Main content ends -->
+
+    <!-- Footer starts -->
+    <div class="footer">
+        <div>
+            <p>All rights reserved.</p>
+        </div>
+    </div>
+    <!-- Footer ends -->
+
+</body>
+</html>
