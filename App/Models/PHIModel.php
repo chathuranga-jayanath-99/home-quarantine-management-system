9365eb8310498d62e811ca4686ba4cd85040254c
Shilpa-Perera <laksahaniperera@gmail.com>
2021-11-21 12:39:11 +0530

    Commit

--- App/Models/PHIModel.php
+++ App/Models/PHIModel.php
@@ -40,4 +40,29 @@ class PHIModel extends \Core\Model{
             return false;
         }
     }
+
+
+    public static function login($email, $password){
+
+        $db = static::getDB();
+
+        $sql = 'SELECT * FROM tbl_phi WHERE email=:email';
+        $stmt = $db->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        $row = $stmt->fetch(PDO::FETCH_OBJ);
+        
+        if(!empty($row)){
+            $hashed_password = $row->password;
+
+            if(password_verify($password, $hashed_password)){
+                return $row;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+
+    }
+
 }
