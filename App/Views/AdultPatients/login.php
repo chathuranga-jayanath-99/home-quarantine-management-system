ae568cc99ef71a7a6dae51e686a72e94c90b5ebd
ThaminduSathruwan <sathruwan99@gmail.com>
2021-11-22 23:57:23 +0530

    Update login.php

--- App/Views/AdultPatients/login.php
+++ App/Views/AdultPatients/login.php
@@ -1,65 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-</head>
+<?php include(APPROOT.'\App\Views\Includes\header.php'); ?>
 
 <body>
-	<section class="h-100">
-		<div class="container h-100">
-			<div class="row justify-content-sm-center h-100">
-				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-					<div class="text-center my-5">
-						<img src="https://www.tenethealth.com/images/global/newsroom-ccb/cardiology-heart-care-technology-doctor.jpg?sfvrsn=befe083e_1" alt="logo" width="100">
-					</div>
-					<div class="card shadow-lg">
-						<div class="card-body p-5">
-							<h1 class="fs-4 card-title fw-bold mb-4">Adult-Patient Login</h1>
-							<form action="<?php echo URLROOT?>/adult-patient/login" method="POST" class="needs-validation" novalidate="" autocomplete="off">
-								
-								<div class="mb-3">
-									<label class="mb-2 text-muted" for="email">Email</label>
-									<input id="email" type="email" class="form-control" name="email" value="<?php echo $data['email']?> " required autofocus>
-									<div class="">
-										<span><?php echo $data['email_err']?></span>
-									</div>
-								</div>
 
-								<div class="mb-3">
-									<div class="mb-2 w-100">
-										<label class="text-muted" for="password">Password</label>
-									</div>
-									<input id="password" type="password" class="form-control" name="password" value="<?php echo $data['password']?>" required>
-								    <div class="">
-										<span><?php echo $data['password_err']?></span>
-							    	</div>
-								</div>
-
-								<div class="d-flex align-items-center">
-									<button type="submit" class="btn btn-primary ms-auto">
-										Login
-									</button>
-								</div>
-							</form>
-						</div>
-						<div class="card-footer py-3 border-0">
-							<div class="text-center">
-								Don't have an account? <a href="<?php echo URLROOT; ?>/adult-patient/register" class="text-dark">Create One</a>
-							</div>
-						</div>
-					</div>
-					<div class="text-center mt-5 text-muted">
-						Copyright &copy; Code Devours 
-					</div>
-				</div>
+    <section class="vh-100" style="background-color: #eee;">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-12 col-xl-10">
+            <div class="card text-black" style="border-radius: 25px;">
+                <div class="card-body p-md-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+							<h1 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Adult-Patient Login</h1>
+                            <form class="mx-1 mx-md-4" action="<?php echo URLROOT?>/adult-patient/login" method='POST'>
+                                <div class="d-flex flex-row align-items-center mb-4">
+                                <i class="fa fa-envelope fa-lg me-3 fa-fw"></i>
+                                    <div class="form-outline flex-fill mb-0">
+                                        <input class="form-control" type="email" name="email" value="<?php echo $data['email']?>" placeholder="Email" required autofocus>
+                                        <span style="color:red"><?php echo $data['email_err']?></span>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row align-items-center mb-4">
+                                <i class="fa fa-key fa-lg me-3 fa-fw"></i>
+                                    <div class="form-outline flex-fill mb-0">
+                                        <input class="form-control" type="password" name="password" value="<?php echo $data['password']?>" placeholder="Password" required>
+                                        <span style="color:red"><?php echo $data['password_err']?></span>
+                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                    <input type="submit" class="btn btn-primary" value="Log-in">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                            <img src="https://i.pinimg.com/originals/ce/ce/88/cece88244f0eec936f250ba1dc67be67.jpg" width=700 height=700 class="img-fluid" alt="Sample image"> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-5 text-muted">
+				Copyright &copy; Code Devours 
 			</div>
-		</div>
-	</section>
-
-	<script src="js/login.js"></script>
+        </div>
+        </div>
+    </div>
+    </section>
 </body>
 </html>
