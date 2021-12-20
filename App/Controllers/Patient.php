3d539786c3fb49db94a41c0b2ed788d29281ff87
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-12-20 15:37:00 +0530

    ChildPatient - Properly managed sessions

--- App/Controllers/Patient.php
+++ App/Controllers/Patient.php
@@ -75,4 +75,22 @@ abstract class Patient extends \Core\Controller {
         return true;
     }
 
+    protected function checkPHISession() {
+        if (isset($_SESSION['phi_id'])) {
+            return true;
+        } else if (isset($_SESSION['child_id'])) {
+            header('location: '.URLROOT.'/child-patient');
+            die();
+        } else if (isset($_SESSION['doctor_id'])) {
+            header('location: '.URLROOT.'/doctor');
+            die();
+        } else if (isset($_SESSION['adPatient_id'])) {
+            header('location: '.URLROOT.'/adult-patient');
+            die();
+        } else {
+            header('location: '.URLROOT);
+            die();
+        }
+    }
+
 }
