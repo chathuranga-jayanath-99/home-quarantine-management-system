fc768a88226fafff6dbfc0d3e40146281d8c48ce
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-12-31 14:30:46 +0530

    ChildPatient - Added navbar basic options

--- App/Views/ChildPatients/navbar.php
+++ App/Views/ChildPatients/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo URLROOT; ?>/child-patient"><?php echo $_SESSION['child_name']; ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
