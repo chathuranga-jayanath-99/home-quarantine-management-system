c898ebae42d9603c7aae23bce10f9a991b9f6b19
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-01-01 16:33:49 +0530

    StatePattern - Fixed $ instance in state classes

--- App/statePattern/Contact.php
+++ App/statePattern/Contact.php
@@ -5,16 +5,17 @@ namespace App\statePattern;
 use App\Controllers\Patient;
 
 class Contact extends State {
+    private static $instance;
 
     private function __construct() {
         parent::__construct();
     }
 
     public static function getInstance() {
-        if (!isset($instance)) {
-            $instance = new Contact();
+        if (!isset(static::$instance)) {
+            static::$instance = new Contact();
         }
-        return $instance;
+        return static::$instance;
     }
 
     public function nextState($patient) {
--- App/statePattern/Inactive.php
+++ App/statePattern/Inactive.php
@@ -5,17 +5,17 @@ namespace App\statePattern;
 use App\Controllers\Patient;
 
 class Inactive extends State {
-    private $instance;
+    private static $instance;
 
     private function __construct() {
         parent::__construct();
     }
 
     public static function getInstance() {
-        if (!isset($instance)) {
-            $instance = new Inactive();
+        if (!isset(static::$instance)) {
+            static::$instance = new Inactive();
         }
-        return $instance;
+        return static::$instance;
     }
 
     public function nextState($patient) {
