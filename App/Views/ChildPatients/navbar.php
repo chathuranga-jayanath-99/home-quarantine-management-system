a551afec256477e4ea75f5b4c17cfe59ee47578f
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-01-10 18:15:55 +0530

    ChildPatient - Added tooltip to 'blinking' for not recorded patients

--- App/Views/ChildPatients/navbar.php
+++ App/Views/ChildPatients/navbar.php
@@ -39,7 +39,7 @@ if ($last) {
           <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/child-patient/profile"><button class="btn <?php if ($page === 'profile') {echo 'btn-success';} ?>">Profile</button></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo URLROOT; ?>/child-patient/record"><button class="btn <?php if ($page === 'record') {echo 'btn-success';} ?>"><?php if ($notRecorded) { ?><span class="spinner-grow spinner-grow-sm text-danger me-2" id="msg-glow"></span><?php } ?>Record Symptoms</button></a>
+          <a class="nav-link" <?php if ($notRecorded) { ?> data-bs-toggle="tooltip" data-bs-placement="bottom" title="You have not recorded symptoms in last 12 hours" <?php } ?> href="<?php echo URLROOT; ?>/child-patient/record"><button class="btn <?php if ($page === 'record') {echo 'btn-success';} ?>"><?php if ($notRecorded) { ?><span class="spinner-grow spinner-grow-sm text-danger me-2" id="rec-glow"></span><?php } ?>Record Symptoms</button></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT; ?>/child-patient/records-history"><button class="btn <?php if ($page === 'rec-history') {echo 'btn-success';} ?>">Record History</button></a>
@@ -56,7 +56,7 @@ if ($last) {
               Update profile
             </a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/child-patient/edit-med-history"  <?php if ($subPage === 'history') {echo 'style="color: green;"';} ?>>
+            <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/child-patient/edit-med-history"  <?php if ($subPage === 'history') {echo 'style="color: green;"';} ?> >
               Update Medical History
             </a></li>
           </ul>
@@ -76,7 +76,7 @@ if ($last) {
         <a class="nav-link">
           <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo" onclick="removeMsgDiv();">
             <i class="fa fa-bell-o fa-lg me-3 fa-fw"></i>
-            <?php if ($has_msg) { ?><span class="spinner-grow spinner-grow-sm text-warning" id="msg-glow"></span><?php } ?>
+            <?php if ($has_msg) { ?><span class="spinner-grow spinner-grow-sm text-warning" id="msg-glow" <?php if ($notRecorded) { ?> data-bs-toggle="tooltip" data-bs-placement="bottom" title="You have unread Notifications" <?php } ?> ></span><?php } ?>
           </button>
         </a>
       </li>
