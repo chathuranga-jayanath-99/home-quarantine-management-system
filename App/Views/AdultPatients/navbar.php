c0ed3f2c998f8f0698e4a3b6ebf360841eb6717a
ThaminduSathruwan <sathruwan99@gmail.com>
2022-01-13 14:46:02 +0530

    Addeda animation copy

--- App/Views/AdultPatients/navbar.php
+++ App/Views/AdultPatients/navbar.php
@@ -39,7 +39,8 @@ if ($last) {
           <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/adult-patient/profile"><button class="btn <?php if ($page === 'profile') {echo 'btn-success';} ?>">Profile</button></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" <?php if ($notRecorded) { ?> data-bs-toggle="tooltip" data-bs-placement="bottom" title="You have not recorded symptoms in last 12 hours" <?php } ?> href="<?php echo URLROOT; ?>/adult-patient/record"><button class="btn <?php if ($page === 'record') {echo 'btn-success';} ?>"><?php if ($notRecorded) { ?><span class="spinner-grow spinner-grow-sm text-danger me-2" id="rec-glow"></span><?php } ?>Record Symptoms</button></a>        </li>
+          <a class="nav-link" <?php if ($notRecorded) { ?> data-bs-toggle="tooltip" data-bs-placement="bottom" title="You have not recorded symptoms in last 12 hours" <?php } ?> href="<?php echo URLROOT; ?>/adult-patient/record"><button class="btn <?php if ($page === 'record') {echo 'btn-success';} ?>"><?php if ($notRecorded) { ?><span class="spinner-grow spinner-grow-sm text-danger me-2" id="rec-glow"></span><?php } ?>Record Symptoms</button></a>        
+        </li>
         <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT; ?>/adult-patient/records-history"><button class="btn <?php if ($page === 'rec-history') {echo 'btn-success';} ?>">Record History</button></a>
         </li>
@@ -96,7 +97,10 @@ if ($last) {
 <script>
     function removeMsgDiv() {
         var msg_div = document.getElementById('msg-div');
-        if (msg_div != null) msg_div.remove();
+        if (msg_div != null) {
+          msg_div.style.opacity = '0';
+          setTimeout(function(){msg_div.parentNode.removeAdult(msg_div);}, 1000);
+        }
         var msg_glow = document.getElementById('msg-glow');
         if (msg_glow != null) msg_glow.remove();
     }
