40607c0ac49106a2b03535046550be7c2c652e24
ThaminduSathruwan <sathruwan99@gmail.com>
2022-01-17 23:10:47 +0530

    updates

--- App/Views/AdultPatients/post_markdead.php
+++ App/Views/AdultPatients/post_markdead.php
@@ -15,7 +15,7 @@
 <?php
     include 'phi-navbar.php';
 ?>
-    <section class="vh-100" style="background-color: #eee;">
+    <section class="vh-auto pt-5 pb-5" style="background-color: #eee; min-height:100vh;">
     <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-lg-12 col-xl-11">
