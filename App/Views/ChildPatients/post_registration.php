ba7c9e99b76b50eefa34fc1cd2af8950297ecc09
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-01-17 22:48:03 +0530

    ChildPatient - Added basic validation for registration

--- App/Views/ChildPatients/post_registration.php
+++ App/Views/ChildPatients/post_registration.php
@@ -24,12 +24,12 @@
                 <div class="row justify-content-center">
                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                     <h2 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Create an Account</h2>
-                    <form action="<?php echo URLROOT?>/PHI/child-patient/register" method='POST'>
+                    <form action="<?php echo URLROOT?>/PHI/child-patient/register" method='POST' id="reg-form">
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="text" name="name" value="<?php echo $data['name']?>" placeholder="Name" required>
-                                <span style="color:red;"><?php echo $data['name_err']?></span>
+                                <input onclick="checkName()" onblur="checkName()" onchange="checkName()" id="name" class="form-control" type="text" name="name" value="<?php echo $data['name']?>" placeholder="Name" required>
+                                <span id="name_err" style="color:red;"><?php echo $data['name_err']?></span>
                             </div>
                         </div>                   
                         <div class="d-flex flex-row align-items-center mb-4">
@@ -41,22 +41,22 @@
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-envelope fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="email" name="email" value="<?php echo $data['email']?>" placeholder="Email" required>
-                                <span style="color:red;"><?php echo $data['email_err']?></span>
+                                <input onclick="checkEmail()" onblur="checkEmail()" onchange="checkEmail()" class="form-control" id="email" type="email" name="email" value="<?php echo $data['email']?>" placeholder="Email" required>
+                                <span id="email_err" style="color:red;"><?php echo $data['email_err']?></span>
                             </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-key fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="password" name="password" value="<?php echo $data['password']?>" placeholder="Password" required>
-                                <span style="color:red;"><?php echo $data['password_err']?></span>
+                                <input onclick="checkPassword()" onblur="checkPassword()" onchange="checkPassword()" id="password" class="form-control" id="password" type="password" name="password" value="<?php echo $data['password']?>" placeholder="Password" required>
+                                <span id="password_err" style="color:red;"><?php echo $data['password_err']?></span>
                             </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-key fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="password" name="confirm_password" value="<?php echo $data['confirm_password']?>" placeholder="Confirm Password" required>
-                                <span style="color:red;"><?php echo $data['confirm_password_err']?></span>
+                                <input onclick="checkConfPassword()" onblur="checkConfPassword()" onchange="checkConfPassword()" id="confirm_password" class="form-control" type="password" name="confirm_password" value="<?php echo $data['confirm_password']?>" placeholder="Confirm Password" required>
+                                <span id="confirm_password_err" style="color:red;"><?php echo $data['confirm_password_err']?></span>
                             </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-4">
@@ -112,5 +112,103 @@
         </div>
     </div>
     </section>
+    <script src="../../static/js/validation-script.js"></script>
+    <script>
+        document.getElementById("reg-form").addEventListener('submit', submitRoutine);
+
+        function checkName() {
+            var name = document.getElementById("name");
+            var name_err = document.getElementById('name_err');
+            if (!validator.validateName(name.value)){
+                name_err.innerHTML = "Enter a valid name";
+            }
+            else{
+                name_err.innerHTML = ""
+            }
+        }
+
+        function checkEmail() {
+            var email = document.getElementById("email");
+            var email_err = document.getElementById('email_err');
+            if (!validator.validateEmail(email.value)){
+                email_err.innerHTML = "Enter a valid email";
+            }
+            else{
+                email_err.innerHTML = "";
+            }
+        }
+
+        function checkPassword() {
+            var password = document.getElementById('password');
+            var confirm_password = document.getElementById('confirm_password');
+            if (password.value.length == 0) {
+                password_err.innerHTML = "Please enter a password";
+            } else if (password.value.length < 6) {
+                password_err.innerHTML = "Password must be at least 6 characters";
+            }
+        }
+
+        function checkConfPassword() {
+            var password = document.getElementById('password');
+            var confirm_password = document.getElementById('confirm_password');
+            var password_err = document.getElementById('password_err');
+            var confirm_password_err = document.getElementById('confirm_password_err');
+            if (confirm_password.value.length == 0) {
+                confirm_password_err.innerHTML = "Please enter password again";
+            } else if (password.value.length >= 6 && password.value == confirm_password.value){
+                password_err.innerHTML = "";
+                confirm_password_err.innerHTML = "";
+            } else {
+                confirm_password_err.innerHTML = "Passwords do not match";
+            }
+        }
+
+        function submitRoutine(e){
+
+            var password = document.getElementById('password');
+            var confirm_password = document.getElementById('confirm_password');
+            var name = document.getElementById("name");
+            var email = document.getElementById("email");
+
+            var password_err = document.getElementById('password_err');
+            var confirm_password_err = document.getElementById('confirm_password_err');
+            var name_err = document.getElementById('name_err');
+            var email_err = document.getElementById('email_err');
+
+            var err_count = 0;
+
+            if (!validator.validateName(name.value)){
+                name_err.innerHTML = "Enter a valid name";
+                err_count++;
+            }
+            else{
+                name_err.innerHTML = ""
+            }
+
+            if (!validator.validateEmail(email.value)){
+                email_err.innerHTML = "Enter a valid email";
+                err_count++;
+            }
+            else{
+                email_err.innerHTML = "";
+            }
+
+            if (err_count){
+                e.preventDefault();
+            }
+            else{
+                if (password.value == confirm_password.value){
+
+                    password_err.innerHTML = "";
+                }
+                else{
+                    password_err.innerHTML = "Passwords do not match";
+                    e.preventDefault();
+                }
+            }
+            e.preventDefault();
+        }
+
+    </script>
 </body>
 </html>
