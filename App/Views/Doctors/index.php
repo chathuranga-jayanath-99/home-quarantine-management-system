43a7e52de0e2d7c1ee12c99428e965aea3c78b40
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-01-19 10:02:54 +0530

    doctors index view updated

--- App/Views/Doctors/index.php
+++ App/Views/Doctors/index.php
@@ -26,7 +26,7 @@
             <div class="card text-black" style="border-radius: 25px;">
                 <div class="card-body p-md-5">
                     <div class="row justify-content-center">
-                        <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                        <div class=" order-2 order-lg-1">
 
 
                         <?php flash('update_result');?>
@@ -34,15 +34,16 @@
 
                             <h1 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Welcome!</h1>
                             
-
-                            <div class="input-group">
-                                <input type="text" id="search_input" class="form-control form-control-lg" placeholder="Search a patient" onkeyup="showSuggestionToEnter(event)">
-                                <button  id="seacrch_btn" class="input-group-text btn btn-dark btn-lg" type="button" onclick="showSuggestions()">Search</button>
-                                <button class="input-group-text btn btn-dark btn-lg" type="button" onclick="reset()">Clear</button>
-                            </div>
-                            
-                            <div class="">
-                                <p>Matches: <span id="search_result" style="font-weight:bold"></span></p>
+                            <div class="container card mb-5 h-auto col-md-10 col-lg-10 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Search a Patient</h5>
+                                    <input type="text" id="search_input" class="form-control my-3 mx-2" placeholder="Search a patient" onkeyup="showSuggestionToEnter(event)">
+                                    <button class="btn btn-primary mx-2 mb-3" id="seacrch_btn" onclick="showSuggestions()">Search</button>
+                                    <button class="btn btn-primary mx-2 mb-3" onclick="reset()">Clear</button>
+                                    <h6 class="card-subtitle mx-2 my-3 text-muted">Matches:</h6>
+                                    <p class="card-text mx-3"><span id="search_result" style="font-weight:bold"></span></p>
+                                    <a href="#"></a>
+                                </div>
                             </div>
 
                             <div class="row row-cols-1 row-cols-md-2 g-4 mb-5">
@@ -58,6 +59,27 @@
                             
                             </div>
 
+                            <!-- <div class="container h-auto col-md-10 col-lg-10 my-3 text-center">
+                                <nav class="navbar navbar-expand-lg">
+                                    <div class="container-fluid justify-content-center">
+                                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#actions" aria-controls="actions" aria-expanded="true" aria-label="Toggle navigation">
+                                        <span class="btn btn-primary">Actions</span>
+                                        </button>
+                                        <div class="collapse navbar-collapse text-start card bg-success my-2" id="actions">
+                                            <ul class="navbar-nav">
+                                                <li class="nav-item">
+                                                <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/doctor/check-patients"><button class="btn">Check All Patients</button></a>
+                                                </li><li class="nav-item">
+                                                <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/doctor/check-records"><button class="btn">Check Records</button></a>
+                                                </li>
+                                                <li class="nav-item">
+                                                <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/doctor/mark-quarantine-results"><button class="btn">Update Quarantine Periods</button></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div> -->
                             
                         </div>
                     </div>
