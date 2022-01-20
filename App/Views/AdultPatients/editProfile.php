3f9035002492b0e66acef4768020fdd11c5f462b
ThaminduSathruwan <sathruwan99@gmail.com>
2022-01-20 15:56:15 +0530

    Correct Edit Profile Errors

--- App/Views/AdultPatients/editProfile.php
+++ App/Views/AdultPatients/editProfile.php
@@ -26,11 +26,11 @@
                 <div class="row justify-content-center">
                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                     <h2 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Edit Profile</h2>
-                    <form action="<?php echo URLROOT?>/adult-patient/edit-profile" method='POST'>
+                    <form id="log-form" action="<?php echo URLROOT?>/adult-patient/edit-profile" method='POST'>
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="text" name="name" value="<?php echo $data['name']?>" placeholder="Name" required>
+                                <input onchange="nameChange()" class="form-control" type="text" name="name" value="<?php echo $data['name']?>" placeholder="Name" required>
                                 <span style="color:red;"><?php echo $data['name_err']?></span>
                             </div>
                         </div>                   
@@ -43,21 +43,21 @@
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-envelope fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="email" name="email" value="<?php echo $data['email']?>" placeholder="Email" required>
+                                <input onchange="emailChange()" class="form-control" type="email" name="email" value="<?php echo $data['email']?>" placeholder="Email" required>
                                 <span style="color:red;"><?php echo $data['email_err']?></span>
                             </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-mobile fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="text" name="contact_no" value="<?php echo $data['contact_no']?>" placeholder="Contact No" required>
+                                <input onchange="contactNoChange()" class="form-control" type="text" name="contact_no" value="<?php echo $data['contact_no']?>" placeholder="Contact No" required>
                                 <span style="color:red;"><?php echo $data['contact_no_err']?></span>
                             </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fa fa-home fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input class="form-control" type="text" name="address" value="<?php echo $data['address']?>" placeholder="Address" required>
+                                <input onchange="addressChange()" class="form-control" type="text" name="address" value="<?php echo $data['address']?>" placeholder="Address" required>
                                 <span style="color:red;"><?php echo $data['address_err']?></span>
                             </div>
                         </div>
@@ -79,5 +79,33 @@
         </div>
     </div>
     </section>
+    <script>
+        let change = false;
+
+        document.getElementById("log-form").addEventListener('submit', submitRoutine);
+
+        function nameChange(){
+            change = true;
+        }
+
+        function emailChange(){
+            change = true;
+        }
+
+        function contactNoChange(){
+            change = true;
+        }
+
+        function addressChange(){
+            change = true;
+        }
+
+        function submitRoutine(e){
+            if(!change){
+                e.preventDefault();
+            }
+        }
+
+    </script>
 </body>
 </html>
