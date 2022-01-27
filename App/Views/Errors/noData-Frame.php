072d8f5d4f64294b585331a237632487581cf906
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-01-27 09:04:41 +0530

    Errors - Added new views for more errors: Internal error, No data, Request error

---
+++ App/Views/Errors/noData-Frame.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Record - Child Patient - Home Isolation System</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <section class="vh-auto pt-5 pb-5">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-20 col-xl-20">
+                <div class="card text-black" style="border:none;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-7 order-2 order-lg-1">
+                                <h1 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 text-danger">Requested Data Not Found</h1>
+                                <!--h5 class="text-center h5 fw-bold mb-5 mx-1 mx-md-4 mt-4"><?php echo $err; ?></h5-->
+                                <!--div class="justify-content-center text-center">
+                                    <a class="btn btn-warning" href="<?php echo URLROOT; ?>">Back To Home Page</a>
+                                </div-->
+                            <!--/div>
+                            <div class="col-md-8 col-lg-5 col-xl-4 d-flex align-items-center order-1 order-lg-2"-->
+                                <div>
+                                    <div>
+                                        <img src="https://image.freepik.com/free-vector/no-data-concept-illustration_114360-616.jpg" width=400 class="img-fluid" alt="Sample image">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
